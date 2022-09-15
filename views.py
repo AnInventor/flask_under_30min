104bc2746c1369c8883351af0581ec153a86e259
Hari Bala <HariBala@Nowhere.comm>
2022-09-15 21:12:57 +0530

    Added comments

--- views.py
+++ views.py
@@ -1,4 +1,3 @@
-#from django.shortcuts import redirect, render
 from flask import Blueprint, render_template, request, url_for, redirect
 
 views = Blueprint(__name__, "views")
@@ -8,9 +7,10 @@ views = Blueprint(__name__, "views")
 def home():
     return "This is home page"
 
+#pass args
 @views.route("/home")
 def home2():
-    return render_template("index.html", name="Hari")
+    return render_template("index.html", name="AnInventor")
 
 #function parameter
 @views.route("/profile/<username>")
@@ -18,13 +18,13 @@ def profile(username):
     return render_template("index.html", name=username)
 
 #request 
-
 @views.route("/pass")
 def passer():
     args = request.args
     name = args.get('name')
     return render_template("index.html", name=name)
 
+#redirect
 @views.route("/go-to")
 def got_to_home():
     return redirect(url_for("views.home2")) 
